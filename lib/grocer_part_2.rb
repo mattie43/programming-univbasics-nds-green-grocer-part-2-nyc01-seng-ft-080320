b86585c5e295ba8138d95cfdc172e311b9a9d5fc
Matt Ericksen <ericksenm93@ymail.com>
2020-08-03 00:14:11 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,11 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  binding.pry
 end
 
 def apply_clearance(cart)
