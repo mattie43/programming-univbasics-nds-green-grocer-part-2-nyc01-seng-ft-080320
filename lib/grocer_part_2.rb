0637107955750dd04555159773ac00fbf97bc5e0
Matt Ericksen <ericksenm93@ymail.com>
2020-08-03 00:59:06 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -44,6 +44,7 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  binding.pry
   consolidate_cart(cart)
   apply_coupons(cart)
   apply_clearance(cart)
