b75025587b9e8bbe9e771ba75226dc85be129c60
Matt Ericksen <ericksenm93@ymail.com>
2020-08-03 00:31:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -12,9 +12,7 @@ def apply_coupons(cart, coupons)
       binding.pry
       if item[:item] == coupon[:item] && item[:count] >= coupon[:num]
         item[:count] -= coupon[:num]
-        if item[:count] == 0
-          item.index
-        end
+        final_arr << item if item[:count] > 0
       end
     end
   end
